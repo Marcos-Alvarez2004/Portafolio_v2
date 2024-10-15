a03d56c606e948b821fc83e1c7e42b5a3be189b9
Marcos Alvarez <137237087+Marcos-Alvarez2004@users.noreply.github.com>
2024-10-15 01:11:29 -0300

    changues design

---
+++ src/components/data.js
@@ -0,0 +1,65 @@
+export const projects = [
+  {
+    titleCard: "Title Card 1",
+    altImgCard: "alt 1",
+    linkCard: "#",
+    descriptionCard:
+      "Ipsum fugiat earum quidem perferendis optio reprehenderit quae quam animi veritatis iste Lorem ipsum dolor sit amet consectetur adipisicing elit.ipsum dolor sit amet consectetur adipisicing elit.",
+    imgCard: "/hero.svg",
+    tagsTecnology: ["Uno", "Dos", "Tres"],
+  },
+  {
+    titleCard: "Title Card 2",
+    altImgCard: "alt 2",
+    linkCard: "#",
+    descriptionCard:
+      "Ipsum fugiat earum quidem perferendis optio reprehenderit quae quam animi veritatis iste Lorem ipsum dolor sit amet consectetur adipisicing elit.ipsum dolor sit amet consectetur adipisicing elit.",
+    imgCard: "/hero.svg",
+    tagsTecnology: ["Uno", "Dos", "Tres", "Cuatro", "Cinco"],
+  },
+  {
+    titleCard: "Title Card 3",
+    altImgCard: "alt 3",
+    linkCard: "#",
+    descriptionCard:
+      "Ipsum fugiat earum quidem perferendis optio reprehenderit quae quam animi veritatis iste Lorem ipsum dolor sit amet consectetur adipisicing elit.ipsum dolor sit amet consectetur adipisicing elit.",
+    imgCard: "/hero.svg",
+    tagsTecnology: ["Uno", "Dos", "Tres"],
+  },
+  {
+    titleCard: "Title Card 4",
+    altImgCard: "alt 3",
+    linkCard: "#",
+    descriptionCard:
+      "Ipsum fugiat earum quidem perferendis optio reprehenderit quae quam animi veritatis iste Lorem ipsum dolor sit amet consectetur adipisicing elit.ipsum dolor sit amet consectetur adipisicing elit.",
+    imgCard: "/hero.svg",
+    tagsTecnology: ["Uno", "Dos", "Tres", "Cuatro", "Cinco"],
+  },
+  {
+    titleCard: "Title Card 5",
+    altImgCard: "alt 5",
+    linkCard: "#",
+    descriptionCard:
+      "Ipsum fugiat earum quidem perferendis optio reprehenderit quae quam animi veritatis iste Lorem ipsum dolor sit amet consectetur adipisicing elit.ipsum dolor sit amet consectetur adipisicing elit.",
+    imgCard: "/hero.svg",
+    tagsTecnology: ["Uno", "Dos", "Tres"],
+  },
+  {
+    titleCard: "Title Card 6",
+    altImgCard: "alt 6",
+    linkCard: "#",
+    descriptionCard:
+      "Ipsum fugiat earum quidem perferendis optio reprehenderit quae quam animi veritatis iste Lorem ipsum dolor sit amet consectetur adipisicing elit.ipsum dolor sit amet consectetur adipisicing elit.",
+    imgCard: "/hero.svg",
+    tagsTecnology: [
+      "Uno",
+      "Dos",
+      "Tres",
+      "Cuatro",
+      "Cinco",
+      "Uno",
+      "Dos",
+      "Tres",
+    ],
+  },
+];
