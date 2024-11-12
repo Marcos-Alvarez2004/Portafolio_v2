1f29bf970d3f756497414d621d34044085414dff
Marcos Alvarez <137237087+Marcos-Alvarez2004@users.noreply.github.com>
2024-11-12 16:43:07 -0300

    add readme

---
+++ README.md
@@ -0,0 +1,13 @@
+![Logo](/favicon.png)
+
+# Portafolio v2
+
+![PortafolioImg](/portafolio.png)
+
+### Portafolio Personal de Marcos Alvarez
+
+Este proyecto está desarrollado principalmente con Astro como framework, complementado con algunas librerías de React y archivos. Para el diseño y los estilos, utilicé TailwindCSS junto con CSS, mientras que Three.js se empleó para crear el fondo animado. La mayoría de las funciones están implementadas en JavaScript para garantizar una experiencia interactiva y dinámica.
+
+# Contacto
+
+Si deseas contactar conmigo, puedes enviarme un mensaje por [![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/marcosalvarezmedero?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app) o enviame un correo a maaarcosaaalvarez13@gmail.com
--- src/js/data.js
+++ src/js/data.js
@@ -5,7 +5,7 @@ export const projects = [
     linkCard: "https://gaming-eccomerce.onrender.com/",
     descriptionCard:
       "Gaming Eccomerce es una web con sistema C.R.U.D. inspirado en plataformas como Booking o Airbnb, pero enfocado en la venta de periféricos para pc y consolas. El sitio cuenta con una base de datos que permite el registro de usuarios con contraseñas, donde cada usuario principal puede gestionar sus propios elementos de forma independiente. Si otro usuario accede con una cuenta diferente, podrá visualizar los elementos creados por los demas usuarios.",
-    imgCard: "/Proyecto 1.png",
+    imgCard: "/proyecto 1.png",
     tagsTecnology: [
       "Vite",
       "React js",
@@ -23,7 +23,7 @@ export const projects = [
     linkCard: "https://ladingvelada.netlify.app/",
     descriptionCard:
       "Landing page de 'La Velada del Año IV', inspirada en un video del canal Midudev. Es una web desarrollada principalmente hecha Astro, siendo uno de los proyectos en los que más aprendí a utilizar este framework. Los estilos se implementaron con Tailwind CSS y el fondo está creado con Three.js. Incluye varias funciones, como un temporizador y efectos interactivos al hacer scroll.",
-    imgCard: "/Proyecto 2.png",
+    imgCard: "/proyecto 2.png",
     tagsTecnology: ["Astro", "Tailwind Css", "Javascript", "Three js"],
   },
   {
@@ -32,7 +32,7 @@ export const projects = [
     linkCard: "https://portafolioastro.netlify.app/",
     descriptionCard:
       "Portafolio Astro esta desarrollado con Astro, utilizando CSS para los estilos y JavaScript para las animaciones. Incluye un switcher que permite cambiar los colores de diversos elementos de la página, además de un interruptor para alternar entre modo oscuro y modo claro.",
-    imgCard: "/Proyecto 3.png",
+    imgCard: "/proyecto 3.png",
     tagsTecnology: ["Astro", "CSS", "Javascript"],
   },
 ];
